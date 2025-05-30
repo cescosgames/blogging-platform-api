b2f845e58babb17aefda1789eb1687e155e87a86
Cesco <cescosgames@gmail.com>
2025-05-30 10:32:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# blogging platform API assignment
+
+## the goals are
+- understand what restful apis are
+- learn how to create a restful api
+- learn about GET POST PUT PATCH and DELETE
+- learn about status codes and error handling in APIs
+- learn how to perform CRUD operations using an API
+- learn how to work with databases
+
+## initial notes
+Feels like I learned a lot of this stuff already with the last blog assignment EXCEPT for working with remote databases. The recommendation here was to use 
+MongoDB so I think it's about time I learn. 
+
+## Gameplan
+1. Set up the blog
+- Since I already have done this, with full CRUD operations, I will REDO this but without saving locally, instead I will not save any information until the DB step
+- example blog posts for setup will be in public/exPosts/post*x*.json
+2. Set up the database
+- Brand new, need to learn!
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express'; // start with express to build our API
+import path from 'path'; // for setting up  public folder
+import { fileURLToPath } from 'url'; // for using __dirname in modules
+import dotenv from 'dotenv'; // import dotenv to manage env variables
+
+// managing environment variables
+dotenv.config();
+const port = process.env.PORT || 8080; // default to 5173 or use environment variables
+
+// get the directory name to load static folder path
+const __filename = fileURLToPath(import.meta.url); // gives ur our file URL, just like filename in commonJS
+const __dirname = path.dirname(__filename); // pass in our file name to get our dirname
+
+// initialize express
+const app = express();
+
+// set up static folder path
+app.use(express.static(path.join(__dirname, 'public'))); // __dirname doesn't work with es modules! need to add lines
+
+
+
+
+// --- current routes below, will move to router soon ---
+
+// index route
+app.use((req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, 'public/html', 'index.html'));
+});
+
+// 404 for undefined routes
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public/html', '404.html'));
+});
+
+// ------------------------------------------------------
+
+
+
+
+// listening function: REMEMBER, node has a bult in watch feature, similar to nodemon - see package.json dev function
+app.listen(port, () => console.log(`Server started on port ${port}`));
